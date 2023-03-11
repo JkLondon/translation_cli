c88b50785caccd3cbfae9b171444a0ac7859f543
JLondon <mikheev.ii@phystech.edu>
2023-03-11 22:29:56 +0300

    fixes

--- main.go
+++ main.go
@@ -1,18 +1,8 @@
 package main
 
 import (
-	"log"
-
 	"github.com/JkLondon/translation_cli/cmd"
-	"github.com/joho/godotenv"
 )
-
-func init() {
-	// loads values from .env into the system
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-}
 
 func main() {
 	cmd.Execute()
